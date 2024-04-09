4af652ae1077c049d6c257d362533d7251ee6ae2
yuto ichinose <yuto.0526.sano@gmail.com>
2024-04-09 11:21:05 +0900

    feat: 変数について

--- main.rs
+++ main.rs
@@ -1,4 +1,16 @@
 fn main() {
     // 世界よ、こんにちは
     println!("Hello, world!!");
+
+    // 変数
+    let age = 22;
+    println!("I am {} years old.", age);
+    // 再代入はできない
+    // age = 21
+
+    // 可変変数で再代入可能
+    let mut name = "John";
+    println!("I am {}!", name);
+    name = "Jane";
+    println!("She is {}!", name);
 }
