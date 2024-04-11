89aa61ca6ceaf65ef8f3712b376b8d2f7d231580
yuto ichinose <yuto.0526.sano@gmail.com>
2024-04-11 12:57:56 +0900

    feat: if文追加

--- main.rs
+++ main.rs
@@ -17,4 +17,10 @@ fn main() {
     // letで再宣言は可能
     let age = 21;
     println!("He is {} years old.", age);
+
+    if age >= 20 {
+        println!("{}歳は成人です。", age)
+    } else {
+        println!("{}歳は未成年です。", age)
+    }
 }
